5ae33cfc9dea63e50eb944a710a53725c55a7e87
Nathaniel Strange <ns2685@bath.ac.uk>
2025-03-05 14:48:06 +0000

    Initial commit with Sudoku files

---
+++ main.py
@@ -0,0 +1,25 @@
+from Dancing_Links_Algorithm import sudoku_solver
+
+def main():
+    sudoku_grid = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0]
+    ]
+
+    solved = sudoku_solver(sudoku_grid)
+
+    if solved[0][0] == -1:
+        print("Could not solve sudoku")
+    else:
+        print("Solved Sudoku:")
+        print(solved)
+
+if name == "__main__":
+    main():
